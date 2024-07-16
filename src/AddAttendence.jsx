18bf6555177d968b14a3be16b270ed75b7c2154f
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-07-16 19:28:19 +0530

    feat: install dependencies and fix issue with webvitals

---
+++ src/AddAttendence.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AddAttendence = () => {
+    return (
+        <div>AddAttendence</div>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { AddAttendence } from "./AddAttendence";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1>Learn Redux</h1>
+      <AddAttendence />
+    </div>
+  );
+}
